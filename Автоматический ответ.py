e7ef5dfb344bd04dabff5be2eed67f544814fd29
darkJs <damirfaz239@gmail.com>
2025-03-14 09:18:33 +0300

    version 1

--- Автоматический ответ.py
+++ Автоматический ответ.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/answer', methods=['POST'])
 def answer():
     data = {
