2858571f0a6bfcb349a24517d6c839cdc6745127
darkJs <damirfaz239@gmail.com>
2025-03-14 08:37:06 +0300

    version 1

---
+++ Тренировки в полёте.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+html = """
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Подготовка к полету на Марс</title>
+</head>
+<body>
+    {% if specialty == 'инженер' or specialty == 'строитель' %}
+        <h2>Инженерные тренажеры</h2>
+        <img src="/static/t1.jpg" alt="Схема расположения инженерных тренажеров">
+    {% else %}
+        <h2>Научные симуляторы</h2>
+        <img src="/static/t2.jpg" alt="Схема расположения научных симуляторов">
+    {% endif %}
+</body>
+</html>
+"""
+
+
+@app.route('/training/<string:prof>')
+def training(prof):
+    return render_template_string(html, specialty=prof)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
